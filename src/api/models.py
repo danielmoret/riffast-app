ab4dfcaafd4c728b818f864803924749f0efc54a
luisjaas <luisjaas98@gmail.com>
2022-11-21 23:44:11 +0000

    tablaticket

--- src/api/models.py
+++ src/api/models.py
@@ -2,26 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+#     def __repr__(self):
+#         return f'<User {self.email}>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
 
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     numero = db.Column(db.Integer, nullable=False)
-    talonario_id = db.Column(db.Integer, ForeignKey('user_talonario.id'))
+    talonario_id = db.Column(db.Integer, ForeignKey('talonario.id'))
     user_ticket_id = db.Column(db.Integer, ForeignKey('user_ticket.id'))
 
     def serialize(self):
