3acce5071992c28896ff3dd041cac4ba88233ad0
estefaniggt <estefanigabri@gmail.com>
2022-11-21 23:37:34 +0000

    Realice el Talonario en models.py

--- src/api/models.py
+++ src/api/models.py
@@ -8,12 +8,29 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+class Talonario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(200), unique=False, nullable=False)
+    premio = db.Column(db.String(200), unique=False, nullable=False)
+    precio = db.Column(db-Float(10), unique=False, nullable=False)
+    imagen_premio = db.Column(db.String(200), unique=False, nullable=False)
+    descripcion = db.Column(db.String(300), unique=False, nullable=False)
+    fecha_sorteo = db.Column(db.dateTime, nullable=False)
+    plataforma_sorteo = db.Column(db.String(100), unique=False, nullable=False)
+    metodo_de_pago = db.Column(db.String(100), unique=False, nullable=False)
+    user_talonario_id = db.Column(db.Integer, ForeignKey('user_talonario.id'))
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "nombre": self.nombre,
+            "premio": self.premio,
+            "precio": self.precio,
+            "imagen_premio": self.imagen_premio,
+            "descripcion": self.descripcion,
+            "fecha_sorteo": self.fecha_sorteo,
+            "plataforma_sorteo": self.plataforma_sorteo,
+            "metodo_de_pago": self.metodo_de_pago,
+            "user_talonario_id": self.user_talonario_id
             # do not serialize the password, its a security breach
         }
