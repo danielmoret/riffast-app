72a2314a92fcab79d80babfae5818bfc037ae6a2
danielmoret <dmoret17@gmail.com>
2022-11-23 12:34:25 +0000

    ruta user_ticket añadida

--- src/api/models.py
+++ src/api/models.py
@@ -61,7 +61,7 @@ class Talonario(db.Model):
 class User_ticket(db.model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(20), unique=True, nullable=False)
+    phone = db.Column(db.String(20),nullable=False)
     email = db.Column(db.String(100))
 
     def serialize(self):
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Ticket, User_talonario, Talonario, User_ticket
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+#Daniel
+#Login user_ticket
+@api.route("/login-ticket", methods=["POST"])
+def login_ticket():
+    email = request.json.get("email", None)
+    phone = request.json.get("phone", None)
+    
+    user_phone = User_ticket.query.filter_by( phone = phone).first()
+    if user_phone is None:
+        user_email = User_ticket.query.filter_by( phone = phone).first()
+        if user_email is None:
+            return jsonify({"msg": "El telefono o email no son correctos"}), 401
+    
+    return jsonify({"msg": "Login ticket satisfactorio"}), 401
+
+#Signup user_ticket
+@api.route('/user-ticket', methods=['GET','POST'])
+def get_users():
+    if request.method == 'GET':
+        users = User_ticket.query.all()
+        users_dictionaries = []
+
+        for user in users:
+            users_dictionaries.append(user.serialize())
+
+        return jsonify(users_dictionaries), 200
+    
+    new_user_data = request.json
+    try:
+        if "full_name" not in new_user_data or new_user_data["full_name"] == "": 
+            raise Exception("No ingresaste el nombre completo",400)
+        if "phone" not in new_user_data or new_user_data["phone"] == "": 
+            raise Exception("No ingresaste el phone",400)
+        new_user = User_ticket.create(**new_user_data)
+        return jsonify(new_user.serialize()),200
+    except Exception as error:
+        return jsonify(error.args[0]),error.args[1] if len(error.args) > 1 else 500
+    
+  
+
