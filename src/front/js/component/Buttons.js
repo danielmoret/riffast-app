4d0dc6b269f455e313bf22f00c28740c57a43ff5
Abrahamjf <abrahamjferrer@gmail.com>
2022-11-28 23:22:20 +0000

    :lipstick: Se agregaron estilos al modal

--- src/front/js/component/Buttons.js
+++ src/front/js/component/Buttons.js
@@ -5,49 +5,58 @@ export const Buttons = (props) => {
   return (
     <div className="container text-center d-flex justify-content-evenly mt-3 ps-2">
       {/* <!-- Button trigger modal --> */}
-      <button type="button" 
-        className="buyer btn btn-lg" 
-        data-bs-toggle="modal" 
-        data-bs-target="#exampleModal">
-          Comprar ticket <i className="fa-solid fa-sack-dollar fa-lg"></i>
+      <button
+        type="button"
+        className="buyer btn btn-lg"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Comprar ticket <i className="fa-solid fa-sack-dollar fa-lg"></i>
       </button>
 
       {/* <!-- Modal --> */}
-      <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              {/* <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1> */}
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
             </div>
             <div className="modal-body">
-              ¿Has concursado en riffast anteriormente?
+              ¿Has concursado en<i>Riffast</i>anteriormente?
             </div>
             <div className="modal-footer">
-              <button type="button" 
-                      className="btn btn-secondary" 
-                      data-bs-dismiss="modal"
-                      onClick={() => props.setBuySelect("newbuy")}>
-                        No</button>
-              <button type="button" 
-                      className="btn btn-primary"
-                      data-bs-dismiss="modal"
-                      onClick={() => props.setBuySelect("previousbuy")}>
-                        si</button>
+              <button
+                type="button"
+                className="yes btn btn-secondary"
+                data-bs-dismiss="modal"
+                onClick={() => props.setBuySelect("newbuy")}
+              >
+                No
+              </button>
+              <button
+                type="button"
+                className="no btn btn-primary"
+                data-bs-dismiss="modal"
+                onClick={() => props.setBuySelect("previousbuy")}
+              >
+                Si
+              </button>
             </div>
           </div>
         </div>
       </div>
-
-
-
-
-
-
-
-
-
-
 
       <button
         type="button"
