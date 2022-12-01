2e779f41bf2b1000c3fbcfc95a1a43539ca31123
danielmoret <dmoret17@gmail.com>
2022-12-01 19:19:17 +0000

    se agrego boton para compartir enlace de la rifa mediante Whatsapp

--- src/front/js/component/BtnCompartir.js
+++ src/front/js/component/BtnCompartir.js
@@ -21,9 +21,10 @@ export const BtnCompartir = (props) => {
 
   return (
     <>
-      <div className="text-center mt-2 d-flex justify-content-center gap-2">
+      <div className="text-center mt-3 d-flex justify-content-center gap-2">
+        <div className="text-center fs-4 fw-bold d-flex">Compartir</div>
         <button className="btn login" onClick={handleClick}>
-          <i class="fa-solid fa-link"></i> Compartir Link
+          <i class="fa-solid fa-link"></i> Link
         </button>
 
         <a
@@ -31,7 +32,7 @@ export const BtnCompartir = (props) => {
           href={`https://twitter.com/intent/tweet?text=Te%20invito%20a%20participar%20en%20mi%20rifa%20en%20https://3000-4geeksacade-reactflaskh-oq5jaeh2ojf.ws-us77.gitpod.io/buy/${props.talonarioID}%20via%20@Riffast`}
           target="_blank"
         >
-          <i className="fa-brands fa-twitter"></i> Comparte en Twitter
+          <i className="fa-brands fa-twitter"></i> Twitter
         </a>
 
         <a
@@ -39,7 +40,14 @@ export const BtnCompartir = (props) => {
           target="_blank"
           href={`https://www.facebook.com/sharer/sharer.php?u=https%3A//3000-4geeksacade-reactflaskh-oq5jaeh2ojf.ws-us77.gitpod.io/buy/${props.talonarioID}`}
         >
-          <i className="fa-brands fa-facebook"></i> Comparte en Facebook
+          <i className="fa-brands fa-facebook"></i> Facebook
+        </a>
+        <a
+          className="btn login"
+          target="_blank"
+          href={`https://wa.me/?text=Te+invito+a+participar+en+mi+rifa+en+https%3a%2f%2f3000-4geeksacade-reactflaskh-oq5jaeh2ojf.ws-us77.gitpod.io%2fbuy%2f${props.talonarioID}`}
+        >
+          <i class="fa-brands fa-whatsapp fa-lg"></i> Whatsapp
         </a>
       </div>
     </>
